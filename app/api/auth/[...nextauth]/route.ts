8ec852ec8e35831b95477c85880abefb060cde1a
gruenberger <andrewgruenberger@gmail.com>
2023-11-10 22:10:45 -0500

    Initial config for security. not done yet. probably bshould have made this a branch.

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,18 @@
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
+import { NextAuthOptions } from "next-auth";
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
+const prisma = new PrismaClient();
+
+export const authOptions: NextAuthOptions = {
+    adapter: PrismaAdapter(prisma),
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GITHUB_ID!,
+            clientSecret: process.env.GITHUB_SECRET!,
+        }),
+    ],
+};
+
