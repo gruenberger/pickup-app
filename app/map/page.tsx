b6b504e787ae12b83ed1ab82c20fe3d2244eeb81
Brian J. Eggleston <bje301@gmail.com>
2023-11-10 21:51:51 -0500

    initial map stuff

--- app/map/page.tsx
+++ app/map/page.tsx
@@ -1,7 +1,25 @@
+import { Wrapper } from "@googlemaps/react-wrapper";
+import React,{ useEffect, useRef } from "react";
+
+
+function MapComponent(){
+    const ref = useRef<HTMLDivElement | null>(null);
+    let center: google.maps.LatLngLiteral = {lat: -34.397, lng: 150.644};
+    let zoom: number = 4;
+
+
+    useEffect(() => {
+        new window.google.maps.Map(ref.current, {
+            center,
+            zoom 
+        });
+    });
+
+    return <div ref={ref} id="map"/>;
+};
+
+
+
 export default function TheMap() {
-    return (
-        <>
-        MAP HERE!
-        </>
-    );
+    return (<Wrapper apiKey=""><MapComponent/></Wrapper>)
 }
