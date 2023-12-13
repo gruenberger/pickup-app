383bd0c863376a68140defd3960ba020b815b244
gruenberger <andrewgruenberger@gmail.com>
2023-12-12 20:54:43 -0500

    Finished most except for form validation. Added a modal as well with redirect.

--- app/create-event/createEventAction.ts
+++ app/create-event/createEventAction.ts
@@ -1,11 +1,13 @@
 'use server';
 
 import { db } from "@/lib/db";
-import { Event } from "@prisma/client";
+import { CreateEventFormData } from "./EventForm";
 
-export async function createEvent(newEvent: any) {
-    console.log(`DATA: ${newEvent}`);
-    // await db.event.create({
-    //     data: { ...newEvent }
-    // });
+export async function createEvent(newEvent: CreateEventFormData) {
+    const event = await db.event.create({data:{
+        ...newEvent
+    }
+});
+    console.log(`Event: ${event.id} has been created.`);
+    return event;
 }
